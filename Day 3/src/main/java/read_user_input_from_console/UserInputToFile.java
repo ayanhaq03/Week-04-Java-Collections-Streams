bf46928c169cc1bc171d733e20b99957df38a9d1
Mohd Ayan haque <ayanhaque904@gmail.com>
2025-02-13 22:25:26 +0530

    [Mohd Ayan Haque] Add . Java program to Read User Input from Console

---
+++ Day 3/src/main/java/read_user_input_from_console/UserInputToFile.java
@@ -0,0 +1,26 @@
+package read_user_input_from_console;
+
+import java.io.*;
+
+public class UserInputToFile {
+    public static void main(String[] args) {
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+             FileWriter fw = new FileWriter("src/main/java/read_user_input_from_console/user_data.txt")) {
+
+            System.out.print("Enter your name: ");
+            String name = br.readLine();
+
+            System.out.print("Enter your age: ");
+            int age = Integer.parseInt(br.readLine());
+
+            System.out.print("Enter your favorite programming language: ");
+            String language = br.readLine();
+
+            fw.write("Name: " + name + "\nAge: " + age + "\nFavorite Language: " + language);
+            System.out.println("Data saved successfully!");
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
