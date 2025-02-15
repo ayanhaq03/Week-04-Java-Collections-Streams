d381d66f8342c059ec972f870ad308c6234f833c
Mohd Ayan haque <ayanhaque904@gmail.com>
2025-02-15 21:56:44 +0530

    [Mohd Ayan Haque] Add . program to validate username using regex

---
+++ Day 5/src/main/java/regex/validate_username/validateUsername.java
@@ -0,0 +1,24 @@
+package regex.validate_username;
+
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
+public class validateUsername {
+    public static  void validate(String pattern , String input){
+        boolean ans = Pattern.matches(pattern,input);
+        if(ans){
+            System.out.println("Valid");
+        }else{
+            System.out.println("Invalid");
+        }
+    }
+    public static void main(String[] args) {
+
+        String pattern = "^[a-z A-Z]\\w{5,15}";
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter your username");
+        String input = sc.nextLine();
+        validate(pattern,input);
+
+    }
+}
