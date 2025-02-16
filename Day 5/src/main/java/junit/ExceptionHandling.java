b907cb62859d680fc498e21469b4783f1ecda53a
Mohd Ayan haque <ayanhaque904@gmail.com>
2025-02-16 22:47:18 +0530

    [Mohd Ayan Haque] Add . program for junit testing

---
+++ Day 5/src/main/java/junit/ExceptionHandling.java
@@ -0,0 +1,7 @@
+package junit;
+
+public class ExceptionHandling {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ Day 5/src/main/java/junit/PerformanceTestingUsingTimeout.java
@@ -0,0 +1,7 @@
+package junit;
+
+public class PerformanceTestingUsingTimeout {
+    public static void longRunningTask() throws InterruptedException{
+        Thread.sleep(3000);
+    }
+}
